45173855a23b48a4678b4868f77b08e34875aa01
Unknown <sumanth261094@gmail.com>
2018-07-19 09:23:05 +0530

    User sign in - finding user

--- passport.js
+++ passport.js
@@ -1,10 +1,11 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy ;
 const { ExtractJwt } = require('passport-jwt');
-
+const LocalStrategy = require('passport-local').Strategy;
 const { JWT_SECRET } =  require('./auth/configurations')
 const User = require('./models/user');
 
+//JSON WEB TOKENS STRATEGY
 passport.use(new JwtStrategy({
     jwtFromRequest: ExtractJwt.fromHeader('authorization'),
     secretOrKey: JWT_SECRET
@@ -24,5 +25,24 @@ passport.use(new JwtStrategy({
     }catch(error ){
         done(error, false);
     }
+}
+));
+
+//LOCAL STRATEGY
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}, async(email, password, done)=>{
+    // Find the user given the email
+    const user = await User.findOne({ email });
+
+    // If not found, handle it
+    if(!user){
+        return done(null, false);
+    }
+    // If found, Check whether password is correct 
+
+    // If not, handle it
+
+    // Otherwise return the user
 }
 ));
